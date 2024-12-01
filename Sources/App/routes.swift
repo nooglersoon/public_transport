be8ecdbd0be01966fe21160b2b48510dd5ecfc9c
Fauzi Achmad Bangsa Diria <>
2024-12-01 13:31:56 +0700

    remove unused code

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,12 +8,7 @@ func routes(_ app: Application) throws {
         /// [GET] /stations
         /// Return the list of stations
         stations.get { request -> [Station] in
-            
-            let db = request.db
-            let stations = try await Station.query(on: db).all()
-            
-            print(stations)
-            
+            let stations = try await Station.query(on: request.db).all()
             return stations
         }
         
@@ -57,62 +52,6 @@ func routes(_ app: Application) throws {
                 
                 return "ok"
             }
-        }
-    }
-    
-    /// [GET] mrt/station/:id
-    /// Return the list of mrt stations
-    
-    /// MRT Stations Routes
-    app.group("mrt","stations") { stations in
-        /// [GET] mrt/stations
-        /// Return the list of mrt stations
-        stations.get { request -> [MRTStationResponse] in
-            
-            // Return specific corridor/section
-            let corridor = request.query[String.self, at: "corridor"]
-            
-            //            if let corridor {
-            //                if corridor == "north_south" {
-            //                    return "Kota to Fatmawati"
-            //                } else if corridor == "east_west" {
-            //                    return "Balaraja to Medan Satria"
-            //                }
-            //            }
-            
-            return [
-                .init(id: UUID(), name: "Bundaran HI", location: .init(latitude: 0.0, longitude: 0.0), lines: [.init(name: "north_south", color: "Red")])
-            ]
-        }
-        
-        /// [POST] mrt/stations
-        /// Add new station
-        stations.post { request in
-            let mrtStationRequest = try request.content.decode(MRTStationRequest.self)
-            return "New MRT Station Added with name: \(mrtStationRequest.name)"
-        }
-    }
-    
-    app.group("mrt","station",":id") { station in
-        
-        /// [GET] mrt/stations/:id
-        /// Get Specific MRT with the given id
-        station.get { request in
-            // early checking to makesure the given id match with the specific type based on the requirement
-            guard let stationID = request.parameters.get("id", as: Int.self) else {
-                throw Abort(.badRequest, reason: "Invalid station id")
-            }
-            return stationID
-        }
-        
-        /// [PUT] mrt/stations/:id
-        /// Update Specific MRT with the given id
-        station.put { request in
-            // early checking to makesure the given id match with the specific type based on the requirement
-            guard let stationID = request.parameters.get("id", as: Int.self) else {
-                throw Abort(.badRequest, reason: "Invalid station id")
-            }
-            return stationID
         }
     }
 }
