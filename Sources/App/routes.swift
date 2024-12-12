6e5443efbce5e976330bf2ad7b0683c0c70f3c4f
Fauzi Achmad Bangsa Diria <>
2024-12-12 22:54:55 +0700

    optimize code

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,23 +5,11 @@ func routes(_ app: Application) throws {
         
     /// Public Stations
     app.group("stations") { stations in
-        
         stations.group(":id") { station in
             
             /// [PUT] /stations/:id
             /// Edit a station's details
             station.put { request -> String in
-                
-                guard let id = request.parameters.get("id") else {
-                    throw Abort(.badRequest, reason: "Missing station id")
-                }
-                
-                return "ok"
-            }
-            
-            /// [DELETE] /stations/:id
-            /// Delete a station
-            station.delete { request -> String in
                 
                 guard let id = request.parameters.get("id") else {
                     throw Abort(.badRequest, reason: "Missing station id")
